5b6a728d5a459eb39d164fcdd750e43f6cdb45eb
DHAPARK <pdh4182@gmail.com>
2022-11-26 06:44:12 +0900

    노드서버만지는중

--- nodejsServer/app.js
+++ nodejsServer/app.js
@@ -1,40 +1,52 @@
 import express from "express";
 import axios from "axios";
-import { initializeApp } from "firebase/app";
-//import { getAnalytics } from "firebase/analytics";
-
+import mysql from "mysql";
+import res from "express/lib/response";
 const app = express();
-const firebaseConfig = {
-  apiKey: "AIzaSyChdzmCfI-uxsk9dl3B4K9ZQNWYwAWRc7c",
-  authDomain: "autosetalarmapp.firebaseapp.com",
-  projectId: "autosetalarmapp",
-  storageBucket: "autosetalarmapp.appspot.com",
-  messagingSenderId: "17425099202",
-  appId: "1:17425099202:web:312aee6efb95af16008e5b",
-  measurementId: "G-WPZGCLMFM6",
-};
 
-const app2 = initializeApp(firebaseConfig);
-
-import * as admin from "firebase-admin";
-import { Firestore } from "firebase-admin/firestore";
-import serviceAccount from "./mydbConfig.json";
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+// 연결할 DB 정보입력
+const connection = mysql.createConnection({
+  host: "44.211.218.78",
+  user: "dummy1",
+  password: "1234",
+  database: "jejuDB",
+  port: "3306",
 });
 
-const db = Firestore.getFirestore();
+connection.query(
+  "create table lightValueTable (idx INT NOT NULL PRIMARY KEY , value INT NOT NULL);",
+  (error, results, fields) => {
+    if (error) throw error;
+    console.log(results);
+  }
+);
 
-async function test(lV) {
-  db.collection("lightValue").doc("lV").set({
-    lightValue: lV,
+connection.query(
+  "INSERT INTO lightValueTable (idx , value) VALUE (?,?)",
+  [1, 0],
+  () => {
+    return;
+  }
+);
+
+async function setLightValueFromMysqlDB( lV ) {
+  connection.query(
+    "UPDATE lightValueTable SET value = ? WHERE idx = ?",[lV,1],(error, results) => {if (error) throw error;res.send({results:results});}
+  );
+}
+
+async function getLightValueFromMysqlDB() {
+  connection.query("SELECT value from lightValueTable where idx = 1", (error, results, fields) => {
+    if (error) throw error;
+    response.send(
+        lV: results,
+    );
   });
 }
 
 app.get("/getLightValue", async (req, res) => {
   const data = await axios.get("http://127.0.0.1:5000/getLightValue");
-  await test(data.data.lightValue);
+
   res.send({ lightValue: data.data.lightValue });
 });
 

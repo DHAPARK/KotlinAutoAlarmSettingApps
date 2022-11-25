caff15090998eeda30b857c3a590ca772c2fd946
DHAPARK <pdh4182@gmail.com>
2022-11-26 06:03:54 +0900

    노드서버만지는중

--- nodejsServer/app.js
+++ nodejsServer/app.js
@@ -19,7 +19,7 @@ const analytics = getAnalytics(app2);
 
 //var admin = require("firebase-admin");
 import admin from "firebase-admin";
-import firestore from "firebase-admin/firestore";
+import firestoreDB from "firebase-admin/firestore";
 //import serviceAccount from "./autosetalarmapp-firebase-adminsdk-1ltqu-c9b17d1df8.json";
 //var serviceAccount = require("path/to/serviceAccountKey.json");
 admin.initializeApp({
@@ -41,7 +41,7 @@ admin.initializeApp({
   }),
 });
 
-const db = firestore.getFirestore();
+const db = firestoreDB.getFirestore();
 
 async function test(lV) {
   db.collection("lightValue").doc("lV").set({

2934297a500f66e3e78d2e3841ed8be1f310fbc7
DHAPARK <pdh4182@gmail.com>
2022-11-26 05:19:16 +0900

    노드서버만지는중

---
+++ nodejsServer/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+app.get("/getLightValue", async (req, res) => {
+  const data = await axios.get("http://127.0.0.1:5000/getLightValue").data;
+  console.log(`전송된 데이터 ${data}`);
+});
+
+app.listen(80, () => {
+  console.log(`80번 포트로 서버 대기중...`);
+});

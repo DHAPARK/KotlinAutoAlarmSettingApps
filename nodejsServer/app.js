1c819aead922b12f235a78f17ba82bd31fb08d76
DHAPARK <pdh4182@gmail.com>
2022-11-26 05:33:37 +0900

    노드서버만지는중

--- nodejsServer/app.js
+++ nodejsServer/app.js
@@ -4,8 +4,10 @@ import axios from "axios";
 const app = express();
 app.get("/getLightValue", async (req, res) => {
   const data = await axios.get("http://127.0.0.1:5000/getLightValue");
-  console.log(`전송된 데이터 ${JSON.stringify(data)}`);
+  console.log(`전송된 데이터 ${data}`);
+  console.log(data);
   //res.json(data);
+  return;
 });
 
 app.listen(3000, () => {

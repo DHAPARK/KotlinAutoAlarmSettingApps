d241659cedec07296740e547b5a1ef1423ddbdf7
DHAPARK <pdh4182@gmail.com>
2022-11-26 07:02:22 +0900

    노드서버만지는중

--- nodejsServer/app.js
+++ nodejsServer/app.js
@@ -11,7 +11,7 @@ const connection = mysql.createConnection({
   database: "jejuDB",
   port: "3306",
 });
-
+/*
 connection.query(
   "create table lightValueTable (idx INT NOT NULL PRIMARY KEY , value INT NOT NULL);",
   (error, results, fields) => {
@@ -27,8 +27,8 @@ connection.query(
     return;
   }
 );
-
-async function setLightValueFromMysqlDB(lV) {
+*/
+async function setLightValueFromMysqlDB(lV, res) {
   connection.query(
     "UPDATE lightValueTable SET value = ? WHERE idx = ?",
     [lV, 1],
@@ -39,21 +39,29 @@ async function setLightValueFromMysqlDB(lV) {
   );
 }
 
-async function getLightValueFromMysqlDB() {
+async function getLightValueFromMysqlDB(res) {
   connection.query(
     "SELECT value from lightValueTable where idx = 1",
     (error, results, fields) => {
       if (error) throw error;
-      response.send({
+      console.log("DB에서 얻어온 조도는 : ");
+      console.log(results);
+      res.send({
         lV: results,
       });
     }
   );
 }
-
-app.get("/getLightValue", async (req, res) => {
+//파이썬 서버에서 주기적으로 조도를 얻어와 db에 저장
+app.get("/setLightValue", async (req, res) => {
   const data = await axios.get("http://127.0.0.1:5000/getLightValue");
+  setLightValueFromMysqlDB(data.data.lightValue, res);
+  res.send({ lightValue: data.data.lightValue });
+});
 
+//안드로이드에서 오는 주기적 요청에 응답
+app.get("/getLightValue", async (req, res) => {
+  getLightValueFromMysqlDB(res);
   res.send({ lightValue: data.data.lightValue });
 });
 
